139d37402b65391230ca8e34a27befae0cae9898
Kristoffer Steen <kristoffer.steen@hotmail.com>
2022-02-23 19:51:05 +0100

    R identified code smells

---
+++ c-sharp/TicTacToe/Board.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+namespace TicTacToe
+{
+    public class Board
+    {
+        private List<Tile> _tiles = new List<Tile>();
+
+        public Board()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    _tiles.Add(new Tile { X = i, Y = j, Symbol = ' ' }); //primitve obsession symbol wrapped constant
+                }
+            }
+        }
+        public Tile TileAt(int x, int y)// primitive obsession
+        {
+            return _tiles.Single(tile => tile.X == x && tile.Y == y);
+        }
+
+        public void AddTileAt(char symbol, int x, int y) // long parameter list
+        {
+            TileAt(x,y).Symbol = symbol; // message chain
+        }
+    }
+}
--- c-sharp/TicTacToe/Game.cs
+++ c-sharp/TicTacToe/Game.cs
@@ -1,127 +1,89 @@
 ﻿using System;
-using System.Linq;
-using System.Collections.Generic;
 
 namespace TicTacToe
 {
     public class Tile
     {
-        public int X {get; set;}
-        public int Y {get; set;}
-        public char Symbol {get; set;}
-    }
-
-    public class Board
-    {
-       private List<Tile> _plays = new List<Tile>();
-       
-        public Board()
-        {
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    _plays.Add(new Tile{ X = i, Y = j, Symbol = ' '});
-                }  
-            }       
-        }
-       public Tile TileAt(int x, int y)
-       {
-           return _plays.Single(tile => tile.X == x && tile.Y == y);
-       }
-
-       public void AddTileAt(char symbol, int x, int y)
-       {
-           var newTile = new Tile
-           {
-               X = x,
-               Y = y,
-               Symbol = symbol
-           };
-
-           _plays.Single(tile => tile.X == x && tile.Y == y).Symbol = symbol;
-       }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public char Symbol { get; set; }
     }
 
     public class Game
     {
         private char _lastSymbol = ' ';
         private Board _board = new Board();
-        
-        public void Play(char symbol, int x, int y)
+        private const char player1 = 'O';
+
+        public void Play(char symbol, int x, int y)// Data clump 
         {
-            //if first move
-            if(_lastSymbol == ' ')
+            //if first move 
+            if (IsFirstMove())
             {
                 //if player is X
-                if(symbol == 'O')
+                if (symbol == player1) //primitive obsession //comment is unclear to what code does
                 {
                     throw new Exception("Invalid first player");
                 }
-            } 
+            }
             //if not first move but player repeated
             else if (symbol == _lastSymbol)
             {
                 throw new Exception("Invalid next player");
             }
-            //if not first move but play on an already played tile
-            else if (_board.TileAt(x, y).Symbol != ' ')
+
+            else if (IsTileTaken(x, y))
             {
                 throw new Exception("Invalid position");
             }
 
             // update game state
             _lastSymbol = symbol;
-            _board.AddTileAt(symbol, x, y);
+            _board.AddTileAt(symbol, x, y);// Data clump 
         }
 
-        public char Winner()
-        {   //if the positions in first row are taken
-            if(_board.TileAt(0, 0).Symbol != ' ' &&
-               _board.TileAt(0, 1).Symbol != ' ' &&
-               _board.TileAt(0, 2).Symbol != ' ')
-               {
-                    //if first row is full with same symbol
-                    if (_board.TileAt(0, 0).Symbol == 
-                        _board.TileAt(0, 1).Symbol &&
-                        _board.TileAt(0, 2).Symbol == 
-                        _board.TileAt(0, 1).Symbol )
-                        {
-                            return _board.TileAt(0, 0).Symbol;
-                        }
-               }
-                
-             //if the positions in first row are taken
-             if(_board.TileAt(1, 0).Symbol != ' ' &&
-                _board.TileAt(1, 1).Symbol != ' ' &&
-                _board.TileAt(1, 2).Symbol != ' ')
-                {
-                    //if middle row is full with same symbol
-                    if (_board.TileAt(1, 0).Symbol == 
-                        _board.TileAt(1, 1).Symbol &&
-                        _board.TileAt(1, 2).Symbol == 
-                        _board.TileAt(1, 1).Symbol)
-                        {
-                            return _board.TileAt(1, 0).Symbol;
-                        }
-                }
+        private bool IsTileTaken(int x, int y)
+        {
+            return GetSymbol(x, y) != ' ';
+        }
 
-            //if the positions in first row are taken
-             if(_board.TileAt(2, 0).Symbol != ' ' &&
-                _board.TileAt(2, 1).Symbol != ' ' &&
-                _board.TileAt(2, 2).Symbol != ' ')
-                {
-                    //if middle row is full with same symbol
-                    if (_board.TileAt(2, 0).Symbol == 
-                        _board.TileAt(2, 1).Symbol &&
-                        _board.TileAt(2, 2).Symbol == 
-                        _board.TileAt(2, 1).Symbol)
-                        {
-                            return _board.TileAt(2, 0).Symbol;
-                        }
-                }
+        private char GetSymbol(int x, int y)
+        {
+            return _board.TileAt(x, y).Symbol;
+        }
 
+        private bool IsFirstMove()
+        {
+            return _lastSymbol == ' ';
+        }
+
+        public char Winner() // Duplicate code // Feature envy //symbol wrapped constant 
+        {
+            for (int rowNumber = 0; rowNumber <= 2; rowNumber++)
+            {
+                if (IsRowTaken(rowNumber) && RowIsFullWithSameSymbol(rowNumber))
+                {
+                    return GetSymbol(rowNumber, 0);
+                }
+            }
             return ' ';
+        }
+
+
+
+        private bool RowIsFullWithSameSymbol(int rowNumber)
+        {
+            return GetSymbol(rowNumber, 0) ==
+                                GetSymbol(rowNumber,1) &&
+                                GetSymbol(rowNumber, 2) ==
+                                GetSymbol(rowNumber, 1);
+        }
+
+        private bool IsRowTaken(int rowNumber)
+        {
+            return IsTileTaken(rowNumber,0) &&
+                           IsTileTaken(rowNumber, 1) &&
+                           IsTileTaken(rowNumber, 2);
         }
     }
 }
